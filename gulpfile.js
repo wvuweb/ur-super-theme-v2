e5675659777b530e1b8250f20beba97d555b20ed
atglenn <adam.glenn@gmail.com>
2016-08-19 09:38:33 -0400

    Initial commit.

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ var concat        = require('gulp-concat');
 // Compile Our Sass from the "scss" directory
 gulp.task('sass', function () {
   gulp.src(['./scss/*.scss','!./scss/_*.scss'])
-    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(sass({outputStyle: 'compressed', sourceComments: 'map'}).on('error', sass.logError))
     .pipe(gulp.dest('./stylesheets'));
 });
 
